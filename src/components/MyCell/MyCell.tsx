734188ac938871e8e02d4cc107b476a3f891fe56
Ilya Iurkov <ilyaiurkov@gmail.com>
2025-08-25 21:58:21 +0200

    deleting inline style in MyCell component, using css modules

--- src/components/MyCell/MyCell.tsx
+++ src/components/MyCell/MyCell.tsx
@@ -14,12 +14,16 @@ interface MyCellProps {
 function MyCell({element, highlightedElements, indexRow, indexColumn, onClick}: MyCellProps) {
     const pieceSymbol = getChessPieceSymbol(element.value);
 
+    const isHighlighted = highlightedElements.some((highlightedElement: IChessPieceMovement) => highlightedElement.row === indexRow && highlightedElement.column === indexColumn);
+
+    const getBackgroundColor = boardColor(indexRow, indexColumn);
+
+    // '#f0d9b5' - white
+    // '#b58863' - black
+
     return (
         <div
-            className={styles.cell}
-            style={{backgroundColor: boardColor(indexRow, indexColumn),
-                ...(highlightedElements.some((highlightedElement: IChessPieceMovement) => highlightedElement.row === indexRow && highlightedElement.column === indexColumn) ? {backgroundColor: 'red'} : {})
-            }} 
+            className={`${styles.cell} ${getBackgroundColor ? styles.white : styles.black} ${isHighlighted ? styles.highlighted : ''}`} 
             onClick={onClick}
         >
             <span className={styles.piece}>
--- src/utils/board-color.ts
+++ src/utils/board-color.ts
@@ -1,3 +1,3 @@
 export const boardColor = (rowIndex: number, columnIndex: number) => {
-    return (rowIndex + columnIndex) % 2 == 0 ? '#f0d9b5' : '#b58863';
+    return (rowIndex + columnIndex) % 2 == 0 ? true : false;
 }
