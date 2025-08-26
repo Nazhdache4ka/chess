b339121a328a3d32238383199cdfb07141670dd0
Ilya Iurkov <ilyaiurkov@gmail.com>
2025-08-26 16:53:56 +0200

    refactor performCastle util

--- src/utils/game-rules/castle-logic/perform-castle.ts
+++ src/utils/game-rules/castle-logic/perform-castle.ts
@@ -1,84 +1,66 @@
 import { ChessPieceTeam, ChessPieceType, type ICastleRights, type IChessBoardElement } from "../../../interfaces";
 
 export const performCastle = (newElements: IChessBoardElement[][], setCastleRights: React.Dispatch<React.SetStateAction<ICastleRights>>, currentPlayer: ChessPieceTeam, fromRow: number, fromColumn: number, sideRookId: string) => {
+    const rookRow = parseInt(sideRookId.split('-')[0]);
+    const rookColumn = parseInt(sideRookId.split('-')[1]);
+    
     if (currentPlayer === ChessPieceTeam.WHITE) {
 
-            if (newElements[7][7].value?.type === ChessPieceType.ROOK && newElements[7][7].value?.team === ChessPieceTeam.WHITE) {
-                const rookRow = parseInt(sideRookId.split('-')[0]);
-                const rookColumn = parseInt(sideRookId.split('-')[1]);
-
+            if (newElements[rookRow][rookColumn].value?.type === ChessPieceType.ROOK && newElements[rookRow][rookColumn].value?.team === ChessPieceTeam.WHITE) {
                 const kingElement = newElements[fromRow][fromColumn];
                 const rookElement = newElements[rookRow][rookColumn];
 
+                if (rookColumn === 7) {
+                    setCastleRights((prev) => ({
+                        ...prev,
+                        white: {
+                            ...prev.white,
+                            kingMoved: true,
+                            rookKingsideMoved: true
+                        }
+                    }));
+                } else {
+                    setCastleRights((prev) => ({
+                        ...prev,
+                        white: {
+                            ...prev.white,
+                            kingMoved: true,
+                            rookQueensideMoved: true
+                        }
+                    }));
+                }
+
+                return {rookElement, kingElement};
+            }
+    }
+
+    if (currentPlayer === ChessPieceTeam.BLACK) {
+        if (newElements[rookRow][rookColumn].value?.type === ChessPieceType.ROOK && newElements[rookRow][rookColumn].value?.team === ChessPieceTeam.BLACK) {
+
+            const kingElement = newElements[fromRow][fromColumn];
+            const rookElement = newElements[rookRow][rookColumn];
+
+            if (rookColumn === 7) {
                 setCastleRights((prev) => ({
                     ...prev,
-                    white: {
-                        ...prev.white,
+                    black: {
+                        ...prev.black,
                         kingMoved: true,
                         rookKingsideMoved: true
                     }
                 }));
-
-                return {rookElement, kingElement};
-            }
-
-            if (newElements[7][0].value?.type === ChessPieceType.ROOK && newElements[7][0].value?.team === ChessPieceTeam.WHITE) {
-                const rookRow = parseInt(sideRookId.split('-')[0]);
-                const rookColumn = parseInt(sideRookId.split('-')[1]);
-
-                const kingElement = newElements[fromRow][fromColumn];
-                const rookElement = newElements[rookRow][rookColumn];
-
+            } else {
                 setCastleRights((prev) => ({
                     ...prev,
-                    white: {
-                        ...prev.white,
+                    black: {
+                        ...prev.black,
                         kingMoved: true,
                         rookQueensideMoved: true
                     }
                 }));
-
-                return {rookElement, kingElement};
             }
-    }
 
-    if (currentPlayer === ChessPieceTeam.BLACK) {
-        if (newElements[0][7].value?.type === ChessPieceType.ROOK && newElements[0][7].value?.team === ChessPieceTeam.BLACK) {
-            const rookRow = parseInt(sideRookId.split('-')[0]);
-            const rookColumn = parseInt(sideRookId.split('-')[1]);
-
-            const kingElement = newElements[fromRow][fromColumn];
-            const rookElement = newElements[rookRow][rookColumn];
-
-            setCastleRights((prev) => ({
-                ...prev,
-                black: {
-                    ...prev.black,
-                    kingMoved: true,
-                    rookKingsideMoved: true
-                }
-            }));
-
-            return {rookElement, kingElement};
-        }
-    }
-
-    if (currentPlayer === ChessPieceTeam.BLACK) {
-        if (newElements[0][0].value?.type === ChessPieceType.ROOK && newElements[0][0].value?.team === ChessPieceTeam.BLACK) {
-            const rookRow = parseInt(sideRookId.split('-')[0]);
-            const rookColumn = parseInt(sideRookId.split('-')[1]);
-
-            const kingElement = newElements[fromRow][fromColumn];
-            const rookElement = newElements[rookRow][rookColumn];
-
-            setCastleRights((prev) => ({
-                ...prev,
-                black: {
-                    ...prev.black,
-                    kingMoved: true,
-                    rookQueensideMoved: true
-                }
-            }));
+            
 
             return {rookElement, kingElement};
         }
