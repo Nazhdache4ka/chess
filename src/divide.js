b4af011d1f88985ccd52b6d6232d45e13137a980
Ilya Iurkov <ilyaiurkov@gmail.com>
2025-09-04 22:14:57 +0200

    фывфыв

---
+++ src/divide.js
@@ -0,0 +1,107 @@
+var divide = function(dividend, divisor) {
+    if (divisor === 1) {
+        if (dividend > (2 ** 31) - 1) {
+            return (2 ** 31) - 1;
+        } else if (dividend < -(2 ** 31)) {
+            return -(2 ** 31);
+        }
+        return dividend;
+    }
+
+    if (divisor === -1) {
+        if (-dividend > (2 ** 31) - 1) {
+            return (2 ** 31) - 1;
+        } else if (-dividend < -(2 ** 31)) {
+            return -(2 ** 31);
+        }
+        return -dividend;
+    }
+
+    const absDividend = Math.abs(dividend);
+    
+    const absDivisor = Math.abs(divisor);
+    
+
+    let result = '';
+    let strDividend = absDividend.toString();
+    let div = ''; // строка, в которую кладется наибоольший разряд делимого
+    let digitPlaceCount = 0; // счетчик, сколько раз делитель помещается в разряд делимого
+    let num; // число, в которое преобразуется разряд делимого
+
+    for (let i = 0; i < strDividend.length; i++) {
+        div = div + strDividend[i];
+        
+        console.log('remain: ' + num + ' itr: ' + i)
+        console.log('div: ' + div + ' itr: ' + i)
+
+        if (Number(div) - absDivisor < 0 && i === 0) {
+            console.log("aboba1")
+            continue;
+        }
+        
+        if (Number(div) - absDivisor < 0 && Number(div)) {
+            result = result + '0';
+            continue;
+        }
+ 
+        num = Number(div);
+
+        while (num > 0) {
+            if (num -  absDivisor < 0) {
+                break;
+            }
+            num = num - absDivisor;
+            digitPlaceCount++;
+        }
+        
+        if (digitPlaceCount) {
+            result = result + digitPlaceCount.toString();   
+        }
+        
+        console.log("result: " + result + " itr: " + i)
+
+        div = num.toString();
+        digitPlaceCount = 0;
+        
+        if (num === 0) {
+            div = '';
+            let accessedEnd = false;
+            for (let j = i + 1; j < strDividend.length; j++) {
+                if (strDividend[j] !== '0') {
+                    break;
+                }
+                if (j === strDividend.length - 1) {
+                    accessedEnd = true;
+                }
+                console.log("plus 0")
+                result = result + '0'
+            }
+            if (accessedEnd) {
+                accessedEnd = false;
+                break;
+            }
+        }
+        
+        // if (Number(num.toString() + strDividend[i + 1]) - absDivisor < 0 && (num !== 0)) {
+        //     result = result + '0'
+        // }
+        
+        console.log("result: " + result)
+    }
+
+    if (divisor < 0 || dividend < 0) {
+        result = -Number(result);
+    }
+    if (divisor < 0 && dividend < 0) {
+        result = -Number(result);
+    }
+
+    if (result > ((2 ** 31) - 1)) {
+        return (2 ** 31) - 1;
+    }
+    if (result < -(2 ** 31)) {
+        return -(2 ** 31);
+    }
+
+    return Number(result);
+};
